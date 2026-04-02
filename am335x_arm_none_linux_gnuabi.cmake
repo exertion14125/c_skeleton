b5a4df5d579c25b6ccde79419b7ebb18fbb4bda3
jgkim <exertion14125@gmail.com>
2026-04-02 06:31:12 +0000

    * Build 환경 추가. 및 수정

--- am335x_arm_none_linux_gnuabi.cmake
+++ am335x_arm_none_linux_gnuabi.cmake
@@ -5,22 +5,21 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
 # Cross toolchain path
-set(CROSS_DIR /opt/toolchain/s5pv210-linaro/gcc/bin)
-set(CMAKE_C_COMPILER ${CROSS_DIR}/arm-linux-gnueabi-gcc)
+set(CROSS_DIR /opt/toolchain/arm-none-linux-gnueabihf-10.3_ARM/gcc/bin)
+set(CMAKE_C_COMPILER ${CROSS_DIR}/arm-none-linux-gnueabihf-gcc)
 
 # # FPU flags (Cortex-A8, VFPv3, softfp ABI)
-# set(FPU_FLAGS "-mcpu=cortex-a8 -mfpu=vfpv3 -mfloat-abi=hard")
-set(FPU_FLAGS "-mfloat-abi=softfp -mfpu=vfpv3")
-# set(FPU_FLAGS "")
+set(FPU_FLAGS "-mcpu=cortex-a8 -mfpu=vfpv3 -mfloat-abi=hard")
+#set(FPU_FLAGS "")
 
 # Compile/Link flags Set (-Wl for linker flags)
 set(CMAKE_C_FLAGS "-Wall -O2 -g ${FPU_FLAGS}" CACHE STRING "C compile flags" FORCE)
-set(CMAKE_EXE_LINKER_FLAGS "-L/opt/toolchain/s5pv210-linaro/gcc/arm-linux-gnueabi/libc/usr/lib" CACHE STRING "Executable linker flags" FORCE)
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -L/opt/toolchain/s5pv210-linaro/sysroot/usr/lib" CACHE STRING "" FORCE)
+set(CMAKE_EXE_LINKER_FLAGS "-L/opt/toolchain/arm-none-linux-gnueabihf-10.3_ARM/gcc/arm-none-linux-gnueabihf/libc/usr/lib" CACHE STRING "Executable linker flags" FORCE)
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -L/opt/toolchain/arm-none-linux-gnueabihf-10.3_ARM/sysroot/usr/lib" CACHE STRING "" FORCE)
  
 # Include/Lib paths
-include_directories(/opt/toolchain/s5pv210-linaro/sysroot/usr/include)
-link_directories(/opt/toolchain/s5pv210-linaro/sysroot/usr/lib)
+include_directories(/opt/toolchain/arm-none-linux-gnueabihf-10.3_ARM/sysroot/usr/include)
+link_directories(/opt/toolchain/arm-none-linux-gnueabihf-10.3_ARM/sysroot/usr/lib)
 
 # [Optional] Default install prefix for cross environment.
 # You can override this at configure time with -DCMAKE_INSTALL_PREFIX=/path
